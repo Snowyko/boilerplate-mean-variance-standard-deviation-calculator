0ee6ab848c768ce73ae44b7a62aa2e86570831be
Lauri Piiroinen <113841099+Snowyko@users.noreply.github.com>
2025-05-14 17:37:33 +0300

    Solution

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,18 @@
 import numpy as np
 
 def calculate(list):
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
 
+    m = np.array(list).reshape(3, 3)
 
-
+    calculations = {
+        'mean': [m.mean(axis=0).tolist(), m.mean(axis=1).tolist(), m.mean().item()],
+        'variance': [m.var(axis=0).tolist(), m.var(axis=1).tolist(), m.var().item()],
+        'standard deviation': [m.std(axis=0).tolist(), m.std(axis=1).tolist(), m.std().item()],
+        'max': [m.max(axis=0).tolist(), m.max(axis=1).tolist(), m.max().item()],
+        'min': [m.min(axis=0).tolist(), m.min(axis=1).tolist(), m.min().item()],
+        'sum': [m.sum(axis=0).tolist(), m.sum(axis=1).tolist(), m.sum().item()],
+    }
 
     return calculations
